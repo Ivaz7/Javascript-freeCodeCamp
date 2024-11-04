cca8224b727b47bdb5eb9091b5e4d1e8cbd9a30f
Ivaz <ivazreza@gmail.com>
2024-11-04 16:52:44 +0700

    Make the javascript full for telephone number validator

---
+++ Telephone-number-validator/phoneNumValidator.js
@@ -0,0 +1,33 @@
+// get every element
+const input = document.querySelector('.input');
+const result = document.querySelector('.result');
+const checkBtn = document.querySelector('.check-btn');
+const clearBtn = document.querySelector('.clear-btn');
+
+// main logic validator
+checkBtn.addEventListener('click', () => {
+  const inputVal = input.value;
+
+  if (!inputVal) {
+    alert('Please provide a phone number');
+    return;
+  };
+
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}$/;
+
+  const rightResultHTML = `<p>Valid US number: ${inputVal}</p>`;
+  const wrongResultHTML = `<p>Invalid US number: ${inputVal}</p>`;
+
+  if (regex.test(inputVal)) {
+    result.innerHTML += rightResultHTML;
+  } else {
+    result.innerHTML += wrongResultHTML;
+  }
+});
+
+
+// clear the result
+clearBtn.addEventListener('click', () => {
+  result.innerHTML = '';
+  input.value = '';
+});
