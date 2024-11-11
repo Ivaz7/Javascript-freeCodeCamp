2a299641fb6c9d846917f9ae0d265037e3574d05
Ivaz <ivazreza@gmail.com>
2024-11-11 11:49:00 +0700

    Make the logic of Cash register project in the changeDueResult function a lot better

--- Cash-register-project/cashRegister.js
+++ Cash-register-project/cashRegister.js
@@ -34,22 +34,23 @@ const listDrawerElem = document.querySelector(".drawer-list");
 drawerCashierList();
 
 function drawerCashierList() {
-  totalCostElem.textContent = `Total cost: $${price}`;
+  totalCostElem.textContent = `Total cost: $${price.toFixed(2)}`;
   listDrawerElem.innerHTML = '<p><strong>Change in drawer:</strong></p>';
   
   for (let i = 0; i < cid.length; i++) {
-    listDrawerElem.innerHTML += `<div>${cid[i][0]} : $${cid[i][1]}</div>`;
+    listDrawerElem.innerHTML += `<div>${cid[i][0]} : $${cid[i][1].toFixed(2)}</div>`;
   }
 }
 
 // main logic when we click
 purchaseBtn.addEventListener('click', () => {
-  let cashVal = Number(cash.value);
+  let cashVal = Math.round(Number(cash.value) * 100);
+  let change = cashVal - Math.round(price * 100);
   let result = [];
   
   changeDueEmpty();
   
-  if (cashVal > totalDrawer()) {
+  if (change > totalDrawer()) {
     changeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS</p>";
     return;
   }
@@ -59,7 +60,7 @@ purchaseBtn.addEventListener('click', () => {
   }
   
   // main logic try to find change
-  changeDueResult(cashVal, result);
+  changeDueResult(result, change);
 
   drawerCashierList();
 
@@ -71,14 +72,14 @@ purchaseBtn.addEventListener('click', () => {
 // make a alert if the cash from the customer less
 // or the customer has the exact cash
 function lessCash(cashVal) {
-  if (cashVal < price) {
+  if (cashVal < price * 100) {
     alert('Customer does not have enough money to purchase the item');
     return true;
   }
   return false;
 }
 function isTheSamePriceCash(cashVal) {
-  if (cashVal === price) {
+  if (cashVal === Math.round(price * 100)) {
     changeDue.innerHTML = "<p>No change due - customer paid with exact cash</p>";
     return true;
   }
@@ -86,7 +87,7 @@ function isTheSamePriceCash(cashVal) {
 }
 // total drawer cash
 function totalDrawer() {
-  return cid.reduce((total, item) => total + item[1], 0);
+  return cid.reduce((total, item) => total + Math.round(item[1] * 100), 0);
 }
 
 // make change-due empty
@@ -95,23 +96,21 @@ function changeDueEmpty() {
 }
 
 // main logic to find the change
-function changeDueResult(cashVal, result) {
-  let change = cashVal - price;
-  
+function changeDueResult(result, change) {
   for (let i = 0; i < coinsValue.length; i++) {
     let cidValSame = cid.find(cid => coinsValue[i][0] === cid[0]);
     let coinName = coinsValue[i][0];
-    let coinValue = coinsValue[i][1];
+    let coinValue = Math.round(coinsValue[i][1] * 100);
     let amountUsed = 0;
 
-    while (change >= coinValue && cidValSame[1] >= coinValue) {
-      cidValSame[1] -= coinValue;
-      change = (change - coinValue).toFixed(2);
+    while (change >= coinValue && cidValSame[1] >= coinValue / 100) {
+      cidValSame[1] = Math.round((cidValSame[1] - coinValue / 100) * 100) / 100;
+      change -= coinValue;
       amountUsed += coinValue;
     }
 
     if (amountUsed > 0) {
-      result.push([coinName, amountUsed.toFixed(2)]);
+      result.push([coinName, (amountUsed / 100).toFixed(2)]);
     }
   }
 }
